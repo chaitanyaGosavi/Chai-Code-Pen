8c902c2c727dee1696c48898dbaac1853e9d996c
chaitanyaGosavi <97080993+chaitanyaGosavi@users.noreply.github.com>
2024-11-24 23:43:28 +0530

    Changed Output BG

--- src/Components/OutputContainer.jsx
+++ src/Components/OutputContainer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const OutputContainer = ({srcDoc}) => {
   return (
     <>
-    <iframe title='Output' className='bg-transparent flex-1 h-1/2' src="" sandbox='allow-scripts' srcDoc= {srcDoc}>
+    <iframe title='Output' className='bg-white flex-1 h-1/2' src="" sandbox='allow-scripts' srcDoc= {srcDoc}>
 
     </iframe>
     </>
