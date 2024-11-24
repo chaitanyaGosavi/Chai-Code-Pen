5e733ccc6fe0f3a38666b6f3c7498fe9467a0270
chaitanyaGosavi <97080993+chaitanyaGosavi@users.noreply.github.com>
2024-11-24 17:56:06 +0530

    Changed Logo and Title size

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NavBar = () => {
     return (
         <div className='w-full h-[7vh] bg-black text-slate-50 flex justify-between items-center'>
-            <div className='w-[30%] lg:w-[20%] uppercase flex items-center justify-around'>
+            <div className='w-[50%] lg:w-[20%] uppercase flex items-center justify-start gap-4'>
                 <a href="#"><img className='w-10 h-10 md:w-12 md:h-12' src="\logoBG.png" alt="logo" /></a>
                 <h4 className='text-xs md:text-lg lg:text-md text-[#f8fdff] font-semibold'>Chai-Code-Pen</h4>
             </div>
